ff73323769b339126098bbfb59f9b1d7810105f2
Jools Clarke <jools.clarke.23@ucl.ac.uk>
2023-11-11 18:19:01 +0000

    fixed interpreter cmd+shift+p and then Python: select interpreter and change to anaconda

--- test_railway.py
+++ test_railway.py
@@ -10,11 +10,11 @@ def test_crs_len_is_3():
         _ = r.Station("aaaa bbbb", "aaaa", "ABCD", 1.,1.,0)
 
  
-'''
+"""
 with pytest.raises(ValueError, match=r'must be \d+$'):
     raise ValueError('value must be 42')
 
 with pytest.raises(ValueError, match='must be 0 or None'):
     raise ValueError('value must be 0 or None')
-'''
+"""
 
