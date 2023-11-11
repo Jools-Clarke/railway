b03b2975ecc64ea00d3febfd3b14fbdb1ce9286e
Jools Clarke <jools.clarke.23@ucl.ac.uk>
2023-11-11 18:07:57 +0000

    created tests, ERROR: pytest not imported? running in wrong terminal somehow? Restarting

---
+++ test_railway.py
@@ -0,0 +1,20 @@
+import railway as r
+import utilities as u
+import pytest
+
+
+
+
+def test_crs_len_is_3():
+    with pytest.raises(ValueError, match=r"'*' is not a valid CRS code, it must contain 3 characters"):
+        _ = r.Station("aaaa bbbb", "aaaa", "ABCD", 1.,1.,0)
+
+ 
+'''
+with pytest.raises(ValueError, match=r'must be \d+$'):
+    raise ValueError('value must be 42')
+
+with pytest.raises(ValueError, match='must be 0 or None'):
+    raise ValueError('value must be 0 or None')
+'''
+
